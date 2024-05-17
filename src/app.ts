42946c780283af819889403e2c7b91708229f376
Angel Velez <angelvelez1415@gmail.com>
2024-05-16 23:07:07 -0500

    Estructura de carpetas inicial, creacion de servidor y configuracion de envs

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log('Hello World');
+import { envs } from "./config/config-barril";
+import { Server } from "./presentation/server";
+
+
+(()=>{
+    main();
+})()
+
+async function main() {
+    //* todo: await de base de datos
+
+    //* todo: inicio de nuestro server
+    new Server({
+        port: envs.PORT,
+    }).start();
+}
---
+++ src/presentation/server.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+
+//creamos un interface para configurar el parametro de port
+
+interface Options{
+    port?: number
+}
+
+export class Server{
+
+    public readonly app = express();
+    private readonly port: number;
+
+    constructor(options:Options){
+        const { port = 3100 } = options;
+        this.port = port;
+    }
+
+    async start(){
+        this.app.listen(this.port, () => {
+            console.log(`Server is running on port ${this.port}`);
+        });
+    }
+}
